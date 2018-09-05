18f1c102c1aed5c58152683d96d68396bc9b15a7
Netanyahu Benjamin <nateakaba2@gmail.com>
2018-09-06 00:54:26 +0300

    I don't care

---
+++ README.txt
@@ -0,0 +1,5 @@
+This is my first project launched on Github.
+
+I AM REALLY excited.
+
+take a look at it.
---
+++ m.java
@@ -0,0 +1,25 @@
+public class m{
+
+	public String measure(String measure1) {
+
+
+    return (measure1);
+
+		
+	}
+    public void c_hlp() {
+
+
+        System.out.println("Measurement:");
+		System.out.println("--------------------");
+		System.out.println("|(1)u |(2)cm| (3)in|");
+		System.out.println("--------------------");
+		System.out.println("|(4)ft|(5)mi| (6)km|");
+		System.out.println("--------------------");
+		System.out.println("|(7)yd|(8)m | (9)mm|");
+		System.out.println("-------------------- ");
+		System.out.print("Measure: ");
+		
+	}
+
+}
